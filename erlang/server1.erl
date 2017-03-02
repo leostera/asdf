6d8f270ad99d1ab8cb2c0e4d3433d486100fb6a3
Leandro Ostera <leandro@ostera.io>
2017-03-02 23:49:52 +0100

    Play around rebuilding OTP's gen_server

---
+++ erlang/server1.erl
@@ -0,0 +1,23 @@
+-module(server1).
+-export([
+        start/2,
+        rpc/2
+        ]).
+
+%% start an erlang process and register it under a name
+start(Name, Mod) ->
+  register(Name, spawn(fun() -> loop(Name, Mod, Mod:init()) end)).
+
+rpc(Name, Request) ->
+  Name ! {self(), Request},
+  receive
+    {Name, Response} -> Response
+  end.
+
+loop(Name, Mod, State) ->
+  receive
+    {From, Request} ->
+      {Response, State1} = Mod:handle(Request, State),
+      From ! {Name, Response},
+      loop(Name, Mod, State1)
+  end.
