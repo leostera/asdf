34c02a7f1e7ce538628a2571d19944cee7f258f9
Leandro Ostera <leandro@ostera.io>
2017-03-29 03:41:32 +0200

    Some scheme stuff

--- scheme/recursion.scm
+++ scheme/recursion.scm
@@ -16,3 +16,18 @@
         ((= kinds-of-coins 3) 10)
         ((= kinds-of-coins 4) 25)
         ((= kinds-of-coins 5) 50)))
+
+; f(n) = n if n < 3
+; f(n) = f(n-1)+2*f(n-2)+3*f(n-3) if n >= 3
+(define (series-for-3s n)
+  (cond ((< n 3) n)
+        ((or (> n 3) (= n 3))
+         (+ (series-for-3s (- n 1))
+            (* 2 (series-for-3s (- n 2)))
+            (* 3 (series-for-3s (- n 3)))))))
+
+(define (series-for-3s-iterative n))
+
+(define (series-for-3s-it i n-1 n-2 n-3 result)
+  (cond (= i 1))
+  (series-for-3s (- n 1) (- n-1 1) (+ n-1 (* 2 n-2) (* 3 n-3))))
