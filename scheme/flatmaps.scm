365b222a66de02f55f0574e8b45bac8a7fdf422d
Leandro Ostera <leandro@ostera.io>
2017-04-13 17:54:24 +0200

    primey

--- scheme/flatmaps.scm
+++ scheme/flatmaps.scm
@@ -1,3 +1,5 @@
+(require math/number-theory)
+
 (define (accumulate op initial sequence)
   (if (null? sequence)
     initial
@@ -23,3 +25,25 @@
                     (permutations (remove x s)))) ; recurse with the rest of the
                                                   ; elements of the set
              s)))
+
+(define (count from to)
+  (define (iter n acc)
+    (if (< n 0)
+      null
+      (cons acc (iter (- n 1) (+ acc 1)))))
+  (iter (- to from) from))
+
+(define (sum-pair n) (+ (car n) (cadr n)))
+
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+             (map (lambda (j) (list i j))
+                  (count 1 (- i 1))))
+           (count 1 n)))
+
+(define (prime-sum? n) (prime? (sum-pair n)))
+
+(define (prime-sums n)
+  (map sum-pair
+       (filter prime-sum?
+               (unique-pairs n))))
